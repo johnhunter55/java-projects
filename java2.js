df64f18483feec396b2e6ccd4a4aa2eb9cce821d
John Hunter <johnroberthunter1@gmail.com>
2026-02-04 10:10:59 -0700

    got the password done

--- java2.js
+++ java2.js
@@ -1,7 +1,23 @@
+// To find area of circle
 function areaCircle(radius) {
     return (radius * radius) * Math.PI;
 }
 
-let radius = areaCircle(1);
+let radius = areaCircle(2);
 
 console.log(radius.toFixed(2));
+
+// get a random password
+let charatureList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*'];
+function randomPassword() {
+    let password = '';
+
+    for (let i = 0; i < 8; i++) {
+        let random = Math.floor(Math.random() * charatureList.length);
+        password += charatureList[random];
+    }
+    return password;
+
+}
+
+console.log(randomPassword())
