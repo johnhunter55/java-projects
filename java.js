ab8101bd296f2df11d4c0956d434aa6c5264c050
John Hunter <johnroberthunter1@gmail.com>
2026-02-17 16:29:09 -0700

    first project

--- java.js
+++ java.js
@@ -1,7 +1,24 @@
 function alertMessage(message) {
-    alert(message);
+  alert(message);
 }
 
+alertMessage("Hello, World!");
+
+const name = "John";
+
 function getWelcomeMessage(name) {
-    console.log
+  return "welcome to my coding world, " + name + "!";
+}
+
+console.log(getWelcomeMessage(name));
+
+const a = 20;
+const b = 10;
+
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
 }
